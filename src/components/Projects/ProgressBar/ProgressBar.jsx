a769938e58a929948d05dc8ddeb3c99b4dd76c07
pranjulpandey22 <pranjulpandey22@gmail.com>
2026-06-26 20:50:50 +0530

    Css fixes

--- src/components/Projects/ProgressBar/ProgressBar.jsx
+++ src/components/Projects/ProgressBar/ProgressBar.jsx
@@ -3,28 +3,32 @@ import { useState, useEffect } from "react";
 const ProgressBar = () => {
   const [progress, setProgress] = useState(0);
   useEffect(() => {
-    const timer = setInterval(() => {
+    const timer = setTimeout(() => {
       setProgress(100);
     }, 100);
-    return () => clearInterval(timer);
-  }, []);
+    return () => clearTimeout(timer);
+  }, [progress]);
   const bars = [10, 30, 50, 70, 100];
   return (
     <>
-      <h1> Progress Bar</h1>
-      {bars.map((ele) => {
+      <h1>Progress Bar</h1>
+      {bars.map((bar) => {
         return (
           <>
             <div className="outer">
               <div
                 className="inner"
                 style={{
-                  width: `${ele}%`,
-                  color: ele <= 11 ? "black" : "white",
+                  width: `${bar}%`,
+                  //width of the inner bar is set to the value of the bar variable,
+                  // which is a percentage of the outer bar's width
+                  color: bar <= 10 ? "black" : "white",
                   transform: `translateX(${progress - 100}%)`,
+                  //to add transition effect, we can use transform property to
+                  // move the inner bar from left to right as the progress increases
                 }}
               >
-                {ele}%
+                {bar}%
               </div>
             </div>
           </>
@@ -34,4 +38,3 @@ const ProgressBar = () => {
   );
 };
 export default ProgressBar;
-``;
