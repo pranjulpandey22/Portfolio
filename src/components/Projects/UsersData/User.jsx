a4da6740a750452af6dc6a8eb4c2533b5a2f3147
pranjulpandey22 <pranjulpandey22@gmail.com>
2026-06-29 20:29:02 +0530

    Css update

--- src/components/Projects/UsersData/User.jsx
+++ src/components/Projects/UsersData/User.jsx
@@ -5,10 +5,18 @@ const User = () => {
   const url = "https://jsonplaceholder.typicode.com/users";
   const [userData, setUserData] = useState([]);
   const [showData, setShowData] = useState(null);
+  const [loading, setloading] = useState(true);
   const fetchApi = async () => {
-    const resposne = await fetch(url);
-    const data = await resposne.json();
-    setUserData(data);
+    try {
+      setloading(true);
+      const resposne = await fetch(url);
+      const data = await resposne.json();
+      setUserData(data);
+    } catch (error) {
+      console.log(error,'error by feching');
+    } finally {
+      setloading(false);
+    }
   };
   useEffect(() => {
     fetchApi();
@@ -16,15 +24,16 @@ const User = () => {
   const handleClick = (user) => {
     setShowData(user);
   };
-  console.log(showData, "heehdh");
+
   return (
     <>
       <h2>User Data Fetch</h2>
-
-      <div className="main">
-        {userData.map((ele) => {
-          return (
-            <>
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="main">
+          {userData.map((ele) => {
+            return (
               <div
                 className="cards"
                 key={ele.id}
@@ -44,39 +53,39 @@ const User = () => {
                   {ele.username}
                 </p>
               </div>
-            </>
-          );
-        })}
-        {showData && (
-          <div className="modal-overlay" onClick={() => setShowData(null)}>
-            <div className="modal" onClick={(e) => e.stopPropagation()}>
-              <button className="close-btn" onClick={() => setShowData(null)}>
-                ✖
-              </button>
+            );
+          })}
+        </div>
+      )}
+      {showData && (
+        <div className="modal-overlay" onClick={() => setShowData(null)}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+            <button className="close-btn" onClick={() => setShowData(null)}>
+              ✖
+            </button>
 
-              <h2>{showData.name}</h2>
-              <p>
-                <strong>Username:</strong> {showData.username}
-              </p>
-              <p>
-                <strong>Email:</strong> {showData.email}
-              </p>
-              <p>
-                <strong>Phone:</strong> {showData.phone}
-              </p>
-              <p>
-                <strong>Website:</strong> {showData.website}
-              </p>
-              <p>
-                <strong>Company:</strong> {showData.company.name}
-              </p>
-              <p>
-                <strong>City:</strong> {showData.address.city}
-              </p>
-            </div>
+            <h2>{showData.name}</h2>
+            <p>
+              <strong>Username:</strong> {showData.username}
+            </p>
+            <p>
+              <strong>Email:</strong> {showData.email}
+            </p>
+            <p>
+              <strong>Phone:</strong> {showData.phone}
+            </p>
+            <p>
+              <strong>Website:</strong> {showData.website}
+            </p>
+            <p>
+              <strong>Company:</strong> {showData.company.name}
+            </p>
+            <p>
+              <strong>City:</strong> {showData.address.city}
+            </p>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </>
   );
 };
