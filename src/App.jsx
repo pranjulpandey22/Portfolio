1b89c9378fa97746a94e6dd99e7f0999638dd4d6
pranjulpandey22 <pranjulpandey22@gmail.com>
2026-07-01 00:06:56 +0530

    TO DO APP

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ProgressBar from "./components/Projects/ProgressBar/ProgressBar";
 import User from "./components/Projects/UsersData/User";
+import Todo from "./components/Projects/Todo/Todo";
 const App =()=>{
   return (
     <>
       <Navbar />
       <h1>Frontend Queries</h1>
+      <Todo />
       {/* <Hero /> */}
       {/* <ProgressBar /> */}
-      <User />
+      {/* <User /> */}
     </>
   )
 }
---
+++ src/components/Projects/Todo/Todo.jsx
@@ -0,0 +1,77 @@
+import "./Todo.css";
+import { useState } from "react";
+
+const Todo = () => {
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([]);
+  const handleClick = () => {
+    const items = {
+      id: Date.now(),
+      name: input.trim(),
+      completed: false,
+    };
+    if (input) {
+      setTodo((prev) => [...prev, items]);
+      // setTodo([...todo,items]) another way
+      setInput("");
+    } else {
+      return null;
+    }
+  };
+
+  const handleDel = (id) => {
+    const deleteItem = todo.filter((ele) => {
+      return ele.id !== id;
+    });
+    setTodo(deleteItem);
+  };
+  const handleCompleteToggle = (id) => {
+    setTodo(
+      todo.map((ele) => {
+        if (ele.id === id) {
+          return {
+            ...ele,
+            completed: !ele.completed,
+          };
+        }
+        return ele;
+      }),
+    );
+  };
+
+  return (
+    <>
+      <h1>Create TO Do List</h1>
+      <div className="center">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="buttonADD" onClick={handleClick}>
+          Add
+        </button>
+        <ul className="hello">
+          {todo.map((ele) => {
+            return (
+              <>
+                <li key={ele.id}>
+                  <input
+                    type="checkbox"
+                    checked={ele.completed}
+                    onChange={() => handleCompleteToggle(ele.id)}
+                  />
+                  <span className={ele.completed ? "linethrough" : ""}>
+                    {ele.name}
+                  </span>
+                  <button onClick={() => handleDel(ele.id)}>Delete</button>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Todo;
